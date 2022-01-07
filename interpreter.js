13c09d9b6a319b10685b5955fdcbfe06837d4ff6
ilias jabri <68255254+iliass1234@users.noreply.github.com>
2022-01-07 03:20:34 +0100

    Update interpreter.js

--- interpreter.js
+++ interpreter.js
@@ -1,7 +1,21 @@
 let text = document.getElementById('interpreter')
 
+
+class Interpreter{
+    read(inputField){
+        let code = inputField.value 
+
+        if(/ktab\(/.test(code)){
+            let txt = code.match(/(?=ktab\()\w+(?<= \=)/)
+            return txt[0]
+         }
+    }
+}
+let interp = new Interpreter()
 document.addEventListener('keydown', function(event) {
     if(event.keyCode == 13) {
-        alert('.............');
+        text.value ='hello'
     }
 });
+
+interp.read(text.value)
