29e41ccd9ebeac6f81708913e908285cc52ea9c9
ilias jabri <68255254+iliass1234@users.noreply.github.com>
2022-01-07 02:40:39 +0100

    Add files via upload

---
+++ interpreter.js
@@ -0,0 +1,7 @@
+let text = document.getElementById('interpreter')
+
+document.addEventListener('keydown', function(event) {
+    if(event.keyCode == 13) {
+        alert('.............');
+    }
+});
