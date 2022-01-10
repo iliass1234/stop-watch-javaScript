6cc8ca624dc22616c4b583b930317fec39f94048
ilias jabri <68255254+iliass1234@users.noreply.github.com>
2022-01-10 03:14:17 +0100

    Update clock.js

--- clock.js
+++ clock.js
@@ -7,6 +7,9 @@ let hoursInput = document.getElementById('hours-input')
 let minInput = document.getElementById('min-input')
 let secInput = document.getElementById('sec-input')
 let setBtn = document.getElementById('input-button')
+let hoursTicker = document.getElementById('hours-indicator')
+let minTicker = document.getElementById('min-indicator')
+let secTicker = document.getElementById('sec-indicator')
 
 let secTick = 0
 let hoursTick = 0
@@ -39,6 +42,7 @@ let clockSpeed = time * 10
             clockWall.style.backgroundColor = 'black'
         }, 2000);
      }
+
     secTick = seccond * (360/60)
     minTick = minute * (360/60)
     hoursTick = hour * (360/12)
@@ -68,22 +72,34 @@ let clockSpeed = time * 10
 }
 
 
+hoursInput.onfocus = function(){
+    hoursTicker.style.backgroundColor = 'yellowgreen'
+}
+minInput.onfocus = function(){
+    minTicker.style.backgroundColor = 'yellowgreen'
+}
+secInput.onfocus = function(){
+    secTicker.style.backgroundColor = 'yellowgreen'
+}
 
 hoursInput.onblur = function(){
     let h = hoursInput.value 
     hoursTick = h * (360/12)
+    hoursTicker.style.backgroundColor = 'white'
     hoursIndicator.style.transform = `rotate(${hoursTick}deg)`
 
 }
 minInput.onblur = function(){
     let h = minInput.value 
     minTick = h * (360/12)
+    minTicker.style.backgroundColor = 'white'
     minIndicator.style.transform = `rotate(${minTick}deg)`
 
 }
 secInput.onblur = function(){
     let h = secInput.value 
     secTick = h * (360/12)
+    secTicker.style.backgroundColor = 'red'
     secIndicator.style.transform = `rotate(${secTick}deg)`
 
 }
@@ -94,18 +110,27 @@ setBtn.onclick = function (){
     let sec = parseInt(secInput.value)
     let min = parseInt(minInput.value)
     let hours = parseInt(hoursInput.value)
+    let h,m,s ;
 
 
     setInitTime(sec,min,hours)
-}
 
+     setInterval(() => {
+         if (secInput.value == 59) {
+             secInput.value = 0
+             minInput.value = parseInt(minInput.value)+1
+         }
+         if (minInput.value == 59 ) {
+            minInput.value = 0
+            hoursInput.value = parseInt(hoursInput.value)+1
+         }
+         if (hoursInput.value  == 12) {
+            hoursInput.value = 0
+         }
+        secInput.value = parseInt(secInput.value) + 1
+        clockAction()
+        
+    }, 1);
 
-
-/* setInterval(() => {
-    clockAction()
-    
-}, 10);
-
- */
-
+}    
 
